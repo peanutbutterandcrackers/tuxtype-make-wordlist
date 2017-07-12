d9a6466559d64bd388d0a2bce6bbd6b41ceef1c4
Prafulla Giri <pratheblackdiamond@gmail.com>
2017-07-12 10:49:37 +0545

    The 'lesson' in tuxtype will now be entitled '$USER [keys: *keyslearnt*]' instead of just $USER, now.

--- makeWordList.sh
+++ makeWordList.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-words_learnt=$1
+words_learnt=$(echo $1 | egrep -o . | sort | tr -d '\n')
 
 build_date=$(date +%F)
 wordListFile="wordList_${build_date}.txt"
 
-echo "$USER" > $wordListFile
+echo "$USER [Keys: ${words_learnt^^}]" > $wordListFile
 
 egrep -i "^[${words_learnt}]{1,}$" /usr/share/dict/words | sort > words.txt
 
