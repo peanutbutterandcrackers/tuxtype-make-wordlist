716b1c5c8fa015d160115db9d6e888322da163f3
Prafulla Giri <pratheblackdiamond@gmail.com>
2017-07-12 10:20:20 +0545

    Initial Commit This is what I came up with for a friend of mine who's learning how to type, yesterday. Need some polishing up, though.

---
+++ makeWordList.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+words_learnt=$1
+
+build_date=$(date +%F)
+wordListFile="wordList_${build_date}.txt"
+
+echo "$USER" > $wordListFile
+
+egrep "^[${words_learnt}asdefjkil]{1,}$" /usr/share/dict/words > words.txt
+
+for word in $(cat words.txt); do
+	echo "${word^^}" >> $wordListFile
+done
+
+rm words.txt
+rm ~/.tuxtype/words/wordList_*.txt
+[[ -d ~/.tuxtype/words ]] || mkdir -p ~/.tuxtype/words
+cp $wordListFile ~/.tuxtype/words/
