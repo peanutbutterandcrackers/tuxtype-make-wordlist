f7cae681d4efa5fd033ad69c0cb72be02720f8d4
Prafulla Giri <pratheblackdiamond@gmail.com>
2017-08-21 08:08:39 +0545

    The Installation Script!

---
+++ INSTALL.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+help () {
+	cat <<- _EOF_
+		Installation Help: tuxtype-make-wordlist
+		-h, --help -> Display this help and exit
+		-g, --global -> Install globally, requires super-user privileges
+		-l, --local -> Install locally, doesn't require elevated privileges
+	_EOF_
+}
+
+check_PATH () {
+	# checks whether or not ~/.local/bin
+	# is in the PATH variable or not
+	echo $PATH | grep --silent $HOME/\.local/bin 
+	if [ $? -ne 0 ]; then
+		{ echo -n "export PATH='"
+		  echo -n $HOME/.local/bin: ;
+		  echo $PATH\' ;
+		} >> ~/.profile 
+		source ~/.profile
+	fi
+}
+
+local_install () {
+	# will reside in ~/.local/bin
+	check_PATH
+	mkdir -p ~/.local/bin
+	cp -a $(dirname $0)/makeWordList.sh ~/.local/bin/tuxtype-make-wordlist
+	echo "Completed local installation successfully."
+}
+
+global_install () {
+	# will reside in /usr/local/bin
+	# requires superuser privileges
+	if [ $(id -u) -eq 0 ]; then
+		cp -a $(dirname $0)/makeWordList.sh /usr/local/bin/tuxtype-make-wordlist
+	else
+		echo "Error: Requires Super User Privileges for global installation."
+		echo "Try local installation instead, or use sudo, or ask your sysadmin."
+		echo "Exiting."
+		exit 1
+	fi
+}
+
+if [[ -z "$1" ]]; then
+	local_install
+	exit
+fi
+
+while [[ -n "$1" ]]; do
+	case $1 in 
+		-h | --help )	help
+		            	exit
+						;;
+		-g | --global )	global_install
+		              	exit
+						;;
+		-l | --local )	local_install
+		                exit
+						;;
+		* )             help
+		                exit 1
+	esac			
+	shift
+done
